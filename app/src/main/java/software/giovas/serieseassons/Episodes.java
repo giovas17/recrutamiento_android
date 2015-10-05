995ea0ce0e82d4e9951140e02da79fcb57134d40
Giovani Gonzalez <darkgeat@gmail.com>
2015-10-05 17:08:56 -0500

    Initial commit

---
+++ app/src/main/java/software/giovas/serieseassons/Episodes.java
@@ -0,0 +1,15 @@
+package software.giovas.serieseassons;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by darkgeat on 10/5/15.
+ */
+public class Episodes extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_seassons);
+    }
+}
